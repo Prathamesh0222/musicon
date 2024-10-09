2c7047f399f8fa963903525705aa9cc6e7859d30
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-10-09 23:04:19 +0530

    Refactor API routes for upvoting and downvoting streams

--- src/app/api/streams/downvote/route.ts
+++ src/app/api/streams/downvote/route.ts
@@ -1,45 +1,67 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
 import prisma from "@/config/prisma.config";
 import { UpvoteSchema } from "@/lib/upvote-validation";
-import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 
 export async function POST(req: Request) {
-    const session = await getServerSession(authOptions);
-    if (!session?.user) {
-        return Response.json({
-            success: "false",
-            message: "Unauthenticated"
-        }, {
-            status: 403
-        })
+  const session = await getServerSession(authOptions);
+  if (!session?.user) {
+    return NextResponse.json(
+      {
+        message: "Unauthenticated",
+      },
+      {
+        status: 403,
+      }
+    );
+  }
+
+  try {
+    const data = UpvoteSchema.parse(await req.json());
+
+    const upvote = await prisma.upvotes.findUnique({
+      where: {
+        userId_streamId: {
+          userId: session.user.id,
+          streamId: data.streamId,
+        },
+      },
+    });
+
+    if (upvote) {
+      return NextResponse.json(
+        {
+          message: "Already upvoted",
+        },
+        {
+          status: 400,
+        }
+      );
     }
-    const body = UpvoteSchema.parse(await req.json());
-    try {
 
+    const res = await prisma.upvotes.delete({
+      where: {
+        userId_streamId: {
+          userId: session.user.id,
+          streamId: data.streamId,
+        },
+      },
+    });
 
-        await prisma.upvotes.delete({
-            where: {
-                userId_streamId: {
-                    userId: session.user.id,
-                    streamId: body.streamId,
-                },
-            },
-        });
-
-        return Response.json({
-            success: true,
-            message: "Upvote removed successfully",
-        }, {
-            status: 200
-        })
-
-    } catch (e) {
-        console.error("Error removing upvote", e);
-        return Response.json({
-            success: false,
-            message: "Error removing upvote"
-        }, {
-            status: 500
-        })
-    }
+    return NextResponse.json({
+      message: "Done!",
+      res,
+    });
+  } catch (e) {
+    console.error("Error :", e);
+    return NextResponse.json(
+      {
+        message: "Error while upvoting",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
--- src/app/api/streams/my/route.ts
+++ src/app/api/streams/my/route.ts
@@ -1,38 +1,62 @@
 import prisma from "@/config/prisma.config";
-import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
 
 export async function GET() {
-    try {
-        const session = await getServerSession(authOptions);
-        if (session?.user) {
-            return Response.json({
-                success: false,
-                message: "Unauthenticated"
-            }, {
-                status: 403,
-            });
+  try {
+    const session = await getServerSession();
+    const user = await prisma.user.findFirst({
+      where: {
+        email: session?.user?.email ?? "",
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          message: "Unauthenticated",
+        },
+        {
+          status: 403,
         }
-
-        const streams = await prisma.stream.findMany({
-            where: {
-                userId: session?.user
-            }
-        });
-
-        return Response.json({
-            success: true,
-            streams
-        }, {
-            status: 200,
-        });
-    } catch (error) {
-        console.error("Error fetching streams:", error);
-        return Response.json({
-            success: false,
-            message: "Internal Server Error"
-        }, {
-            status: 500,
-        });
+      );
     }
+
+    const streams = await prisma.stream.findMany({
+      where: {
+        userId: user.id,
+      },
+      include: {
+        _count: {
+          select: {
+            Upvotes: true,
+          },
+        },
+        Upvotes: {
+          where: {
+            userId: user.id,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json({
+      streams: streams.map(({ _count, ...rest }) => ({
+        ...rest,
+        upvotes: _count.Upvotes,
+        haveUpvoted: rest.Upvotes.length ? true : false,
+      })),
+    });
+  } catch (error) {
+    console.error("Error fetching streams:", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Internal Server Error",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
